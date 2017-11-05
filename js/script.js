52801661e8316634e36457921ea76c1597150ff9
demikhovr <demikhovr@gmail.com>
2017-11-05 17:40:42 +0300

    Добавлена стилизация модальных окон, google maps, мелкие правки

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var searchForm = document.querySelector('.user-nav__search-form');
 var searchInput = document.querySelector('.user-nav__search-input');
 var addCatalog = document.querySelector('.main-nav__add-list');
-
-if (searchInput != null) {
-  searchInput.classList.remove('user-nav__search-input--nojs');
-}
+var modal = document.querySelectorAll('.modal');
+var modalWriteUs = document.querySelector('.modal--write-us');
+var modalMap = document.querySelector('.modal--map');
+var writeUsOpenBtn = document.querySelector('.contacts__btn');
+var mapOpenBtn = document.querySelector('.contacts__map');
+var modalCloseBtn = document.querySelectorAll('.modal__close-btn');
 
 function onInputFocus() {
   searchForm.classList.add('user-nav__search-form--focus');
@@ -12,6 +14,18 @@ function onInputFocus() {
 
 function onInputBlur() {
   searchForm.classList.remove('user-nav__search-form--focus');
+}
+
+function closeWriteUs() {
+  modalWriteUs.classList.remove('modal--show');
+}
+
+function closeMap() {
+  modalMap.classList.remove('modal--show');
+}
+
+if (searchInput != null) {
+  searchInput.classList.remove('user-nav__search-input--nojs');
 }
 
 if (searchInput != null) {
@@ -27,4 +41,28 @@ if (addCatalog != null) {
   addCatalog.addEventListener('blur', function(e) {
     this.classList.toggle('main-nav__add-list--show');
   }, true);
+}
+
+if (modalWriteUs != null) {
+  writeUsOpenBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    modalWriteUs.classList.add('modal--show');
+  });
+}
+
+if (modalMap != null) {
+  mapOpenBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    modalMap.classList.add('modal--show');
+  });
+}
+
+if (modal != null) {
+  for (var i = 0; i < modal.length; i++) {
+    modal[i].addEventListener('click', function(e) {
+      if (e.target.classList.contains('modal__close-btn')) {
+        this.classList.remove('modal--show');
+      }
+    })
+  }
 }
